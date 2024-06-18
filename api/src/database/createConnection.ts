ee3607cb7d129f459a890c53fc0a190ac339b71e
Hemant <hjkumar17@gmail.com>
2024-06-18 13:49:03 +0530

    env changes

--- api/src/database/createConnection.ts
+++ api/src/database/createConnection.ts
@@ -4,11 +4,11 @@ import * as entities from '../entities';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'Postgres@24',
-  database: 'jira_development',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   synchronize: true,
   logging: false,
   entities: Object.values(entities),
